9f3a7d588df451f973a24ca3936ad3ce8a10f7da
Nick McLarty <nick@inick.net>
2013-02-24 01:31:29 -0600

    Version 1.0 and 1.0.1 Releases

---
+++ readme.txt
@@ -0,0 +1,42 @@
+=== Plugin Name ===
+Contributors: mclarty
+Donate link: http://www.inick.net
+Tags: twitter, hashtag, sidebar, feed, widget
+Requires at least: 2.8
+Tested up to: 3.5.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This is a sidebar widget that creates a simple, clean Twitter feed of a specified hashtag.
+
+== Description ==
+
+This is a sidebar widget that creates a simple, clean Twitter feed of a specified hashtag.  This widget is specifically designed to render a clean, lightweight widget that can be completely styled by the user through CSS.
+
+== Installation ==
+
+1. Upload the `twitter-hashtag-feed` directory to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Add the widget to the desired sidebar
+4. Configure the options with your Twitter application data
+5. Enjoy
+
+== Changelog ==
+
+= 1.0.1 =
+* Replaced static HTML code in form with PHP foreach.
+
+= 1.0.0 =
+* First major release.
+
+== Upgrade Notice ==
+
+= 1.0 =
+This is the first release.
+
+== Custom Theming ==
+
+Each widget allows you to specify a div ID for the individual widget. This can be used for applying any desired CSS to the widget. 
+
+All widgets also have the CSS class .twitter-hashtag-feed-widget for uniform styling across all widgets on the site.
--- twitter-hashtag-feed.php
+++ twitter-hashtag-feed.php
@@ -1,81 +1,111 @@
 <?php
 /*
 Plugin Name: Twitter Hashtag Feed Widget
-Version: 1.0.0
-Description: Allows you to add a widget with a Twitter feed using a hashtag search.
+Plugin URI: 
+Description: This is a sidebar widget that creates a simple, clean Twitter feed of a specified hashtag.
+Version: 1.0.1
 Author: Nick McLarty
 Author URI: http://www.inick.net
+License: GPL2
 */
+
+/**
+ * Add Twitter Hashtag Feed Widget to WordPress
+ */
 
 add_action( 'widgets_init', 'register_twitter_hashtag_feed_widget' );
 
 function register_twitter_hashtag_feed_widget() {
 	register_widget( 'Twitter_Hashtag_Feed_Widget' );
-}
+} // function register_twitter_hashtag_feed_widget
 
 class Twitter_Hashtag_Feed_Widget extends WP_Widget {
-	function Twitter_Hashtag_Feed_Widget() {
-		$opts = array(
-			'classname' => 'Twitter_Hashtag_Feed_Widget',
-			'description' => 'Creates a Twitter feed widget using a hashtag search.',
-			);
 
-	$this->WP_Widget(
-		'Twitter_Hashtag_Feed_Widget',
-		'Twitter Hashtag Feed Widget'
+	/**
+	 * Register widget with WordPress
+	 */
+
+	public function __construct() {
+		parent::__construct(
+			'Twitter_Hashtag_Feed_Widget',	// Base ID
+			'Twitter Hashtag Feed Widget',	// Widget Name
+			array( 'description' => __( 'Adds a widget with a Twitter feed using a hashtag search.', 'text_domain' ) )
 		);
-	}
+	} // public function __construct
 
-	function update( $new_instance, $old_instance ) {
-		return $new_instance;
-	}
 
-	function form( $instance ) {
-		$title = esc_attr( $instance['title'] );
-		$hashtag = esc_attr( $instance['hashtag'] );
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from the database.
+	 * @return array Updated safe values to be saved.
+	 */
+
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$args_list = array( 'title', 'divid', 'hashtag', 'consumer_key', 'consumer_secret', 'oauth_token', 'oauth_token_secret', 'results' );
+
+		foreach( $args_list as $arg ) {
+			$instance[$arg] = strip_tags( $new_instance[$arg] );
+		}
+
+		return $instance;
+	} // public function update
+
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 * @param array $instance Previously saved values from the database.
+	 */
+
+	public function form( $instance ) {
+		$args_list = array(	'title' => 'Title',
+							'divid' => 'Widget Div ID',
+							'hashtag' => 'Twitter Hashtag to Search',
+							'consumer_key' => 'Twitter Consumer Key',
+							'consumer_secret' => 'Twitter Consumer Secret',
+							'oauth_token' => 'Twitter OAuth Access Token',
+							'oauth_token_secret' => 'Twitter OAuth Access Token Secret',
+							'results' => 'Number of Results' );
+
+		$title = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : __( 'New title', 'text_domain' );
+		$divid = isset( $instance['divid'] ) ? esc_attr( $instance['divid'] ) : __( 'twitter-hashtag-feed-widget', 'text_domain' );
+		$hashtag = isset( $instance['hashtag'] ) ? esc_attr( $instance['hashtag'] ) : __( '#hashtag', 'text_domain' );
 		$consumer_key = esc_attr( $instance['consumer_key'] );
 		$consumer_secret = esc_attr( $instance['consumer_secret'] );
 		$oauth_token = esc_attr( $instance['oauth_token'] );
 		$oauth_token_secret = esc_attr( $instance['oauth_token_secret'] );
-		$results = esc_attr( $instance['results'] );
+		$results = isset( $instance['results'] ) ? esc_attr( $instance['results'] ) : 5;
 
+		foreach( $args_list as $arg_key => $arg_label ) {
 	?>
-	<p><label for="<?php echo $this->get_field_id( 'title' ); ?>">
-		<?php _e( 'Title:' ); ?> <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" 
-			name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" /></label>
+	<p><label for="<?php echo $this->get_field_id( $arg_key ); ?>">
+		<?php _e( $arg_label ); ?>: <input class="widefat" id="<?php echo $this->get_field_id( $arg_key ); ?>" 
+			name="<?php echo $this->get_field_name( $arg_key ); ?>" type="text" value="<?php echo ${$arg_key}; ?>" /></label>
 	</p>
-	<p><label for="<?php echo $this->get_field_id( 'hashtag' ); ?>">
-		<?php _e( 'Hashtag:' ); ?> <input class="widefat" id="<?php echo $this->get_field_id( 'hashtag' ); ?>" 
-			name="<?php echo $this->get_field_name( 'hashtag' ); ?>" type="text" value="<?php echo $hashtag; ?>" /></label>
-	</p>
-	<p><label for="<?php echo $this->get_field_id( 'consumer_key' ); ?>">
-		<?php _e( 'Twitter Consumer Key:' ); ?> <input class="widefat" id="<?php echo $this->get_field_id( 'consumer_key' ); ?>" 
-			name="<?php echo $this->get_field_name( 'consumer_key' ); ?>" type="text" value="<?php echo $consumer_key; ?>" /></label>
-	</p>
-	<p><label for="<?php echo $this->get_field_id( 'consumer_secret' ); ?>">
-		<?php _e( 'Twitter Consumer Secret:' ); ?> <input class="widefat" id="<?php echo $this->get_field_id( 'consumer_secret' ); ?>" 
-			name="<?php echo $this->get_field_name( 'consumer_secret' ); ?>" type="text" value="<?php echo $consumer_secret; ?>" /></label>
-	</p>
-	<p><label for="<?php echo $this->get_field_id( 'oauth_token' ); ?>">
-		<?php _e( 'Twitter OAuth Token:' ); ?> <input class="widefat" id="<?php echo $this->get_field_id( 'oauth_token' ); ?>" 
-			name="<?php echo $this->get_field_name( 'oauth_token' ); ?>" type="text" value="<?php echo $oauth_token; ?>" /></label>
-	</p>
-	<p><label for="<?php echo $this->get_field_id( 'oauth_token_secret' ); ?>">
-		<?php _e( 'Twitter OAuth Token Secret:' ); ?> <input class="widefat" id="<?php echo $this->get_field_id( 'oauth_token_secret' ); ?>" 
-			name="<?php echo $this->get_field_name( 'oauth_token_secret' ); ?>" type="text" value="<?php echo $oauth_token_secret; ?>" /></label>
-	</p>
-	<p><label for="<?php echo $this->get_field_id( 'results' ); ?>">
-		<?php _e( 'Number of Results:' ); ?> <input class="widefat" id="<?php echo $this->get_field_id( 'results' ); ?>" 
-			name="<?php echo $this->get_field_name( 'results' ); ?>" type="text" value="<?php echo $results; ?>" /></label>
-	</p>
-
 	<?php 
-	}
+		}
+	} // public function form
 
-	function widget( $args, $instance ) {
-		require_once 'twitteroauth/twitteroauth.php';
 
-		$url = 'https://api.twitter.com/1.1/search/tweets.json';
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @params array $args Widget arguments.
+	 * @params array $instance Saved values from database.
+	 */
+
+	public function widget( $args, $instance ) {
+		require_once 'twitteroauth/twitteroauth.php';	// TwitterOAuth Library (courtesy of https://github.com/abraham/twitteroauth)
+
+		$url = 'https://api.twitter.com/1.1/search/tweets.json';	// Twitter REST API v1.1 (https://dev.twitter.com/docs/api/1.1)
+
 		$query = array(	'q' => $instance['hashtag'],
 						'result_type' => 'recent',
 						'count' => $instance['results'],
@@ -83,17 +113,41 @@ class Twitter_Hashtag_Feed_Widget extends WP_Widget {
 
 		$conn = new TwitterOAuth( $instance['consumer_key'], $instance['consumer_secret'], 
 			$instance['oauth_token'], $instance['oauth_token_secret'] );
+
 		$content = $conn->get( $url, $query );
 
-		echo '<h4 class="widgettitle">' . $instance['title'] . '</h4>';
+		echo '<div id="' . $instance['divid'] . '" class="twitter-hashtag-feed-widget">';
+
+		if ( $instance['title'] ) {
+			echo '<h4 class="widgettitle">' . $instance['title'] . '</h4>';
+		}
+
+		echo '<ul>';
 
 		if ( $content->search_metadata->count > 0 ) {
 			foreach( $content->statuses as $status ) {
-				echo '<p>';
-				echo '<a href="https://twitter.com/account/redirect_by_id?id=' . $status->user->id_str . '" target="_blank">';
-				echo $status->user->name . '</a>: ' . $status->text;
-				echo '</p>';
+				$status->text = preg_replace( '/(http[s]?:\/\/[^\s]*)/i', '<a href="$1">$1</a>', $status->text );	// Hyperlink any URLs
+				$status->text = preg_replace( '/(^|\s)@([a-z0-9_]+)/i', '$1<a href="https://twitter.com/$2">@$2</a>', $status->text );	// Hyperlink @mentions
+				$status->text = preg_replace( '/(^|\s)#([a-z0-9_]+)/i', '$1<a href="https://twitter.com/search?q=%23$2&src=hash">#$2</a>', $status->text );	// Hyperlink #hashes
+
+				echo '<li>';
+				echo '<a href="https://twitter.com/' . $status->user->screen_name . '" target="_blank">';
+				echo "@{$status->user->screen_name}</a>: " . $status->text;
+				echo '</li>';
 			}
+		} elseif ( $content->errors ) {
+			$errors = new WP_Error();
+
+			foreach( $content->errors as $error ) {
+				$errors->add( $error->code, $error->message );
+			}
+
+			echo '<div class="error">' . $errors->get_error_message() . '</div>';
+		} else {
+			echo 'No results found.';
 		}
-	}
-}
+
+		echo '</ul></div>';
+	} // public function widget
+
+} // class Twitter_Hashtag_Feed_Widget
